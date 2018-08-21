2432a3b41ac8382c20d00381ad225b37faa8b698
Lucas Lima de Souza <lucas.lima@vizir.com.br>
2018-08-21 19:00:23 -0300

    Loader#images - context of load all image files from a path it return an array of Image class

--- lib/loader.rb
+++ lib/loader.rb
@@ -1,3 +1,28 @@
-class Loader
+require 'image'
 
+class Loader
+  def initialize(path: '', extractor:)
+    @path = "#{path}/**/*{jpg,JPG}"
+    @extractor = extractor
+  end
+
+  def images
+    files = Dir.glob(@path)
+    files.map { |file| image_from file }
+  end
+
+  private
+
+  def image_from(file)
+    data = @extractor.new(file)
+    Image.new(
+      name: split_file_name(file),
+      latitude: data.gps_latitude,
+      longitude: data.gps_longitude
+    )
+  end
+
+  def split_file_name(file)
+    file.split('/').last
+  end
 end
--- spec/lib/loader_spec.rb
+++ spec/lib/loader_spec.rb
@@ -1,11 +1,37 @@
 require 'spec_helper'
+require 'faker'
+require 'loader'
 
 RSpec.describe Loader do
-  subject(:product) {
-    Image.new(
-      name: Faker::File.file_name('data','image','jpg') ,
-      latitude: Faker::Number.number,
-      longitude: Faker::Number.number)
-    }
+  let(:image) do
+    double(
+      'Data',
+      gps_latitude: Faker::Number.number,
+      gps_longitude: Faker::Number.number
+    )
+  end
 
+  let(:extractor) { double('Exif', new: image) }
+
+  subject do
+    Loader.new(
+      path: 'spec/data',
+      extractor: extractor
+    )
+  end
+
+  describe '#images' do
+    context 'load all image files from a path' do
+      it 'return an array of Image class' do
+        images = subject.images
+
+        expect(images).to be_an Array
+        expect(images.length).to eq 2
+        expect(images.first).to be_an Image
+        expect(images.first.name).to eq 'image_a.jpg'
+        expect(images.last).to be_an Image
+        expect(images.last.name).to eq 'image_e.jpg'
+      end
+    end
+  end
 end
